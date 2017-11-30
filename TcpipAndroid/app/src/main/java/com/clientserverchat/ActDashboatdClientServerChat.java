a9a74d4e580ef50dcf80e2675368cb762c16f61a
Prashant <prashant>
2017-11-30 19:10:18 +0530

    First Commmit 18:38

--- TcpipAndroid/app/src/main/java/com/clientserverchat/ActDashboatdClientServerChat.java
+++ TcpipAndroid/app/src/main/java/com/clientserverchat/ActDashboatdClientServerChat.java
@@ -1,19 +1,20 @@
-package com.tcpipandroid;
+package com.clientserverchat;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 
+import com.tcpipandroid.R;
 import com.tcpipandroid.client.ClientMainActivity;
 import com.tcpipandroid.server.ServerMainActivity;
 
-public class DashboardActivity extends AppCompatActivity {
+public class ActDashboatdClientServerChat extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +29,7 @@ public class DashboardActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "ActClient App", Snackbar.LENGTH_LONG).show();
-                startActivity(new Intent(DashboardActivity.this, ClientMainActivity.class));
+                startActivity(new Intent(ActDashboatdClientServerChat.this, ClientActivity.class));
             }
         });
 
@@ -38,7 +39,7 @@ public class DashboardActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "ActServer App", Snackbar.LENGTH_LONG).show();
-                startActivity(new Intent(DashboardActivity.this, ServerMainActivity.class));
+                startActivity(new Intent(ActDashboatdClientServerChat.this, ServerActivity.class));
             }
         });
 
---
+++ TcpipAndroid/app/src/main/java/com/clientserverchat/ClientActivity.java
@@ -0,0 +1,129 @@
+package com.clientserverchat;
+
+/**
+ * Created by prashant.patel on 11/30/2017.
+ */
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.tcpipandroid.R;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+public class ClientActivity extends Activity {
+    EditText serverIp,smessage;
+    TextView chat;
+    Button connectPhones,sent;
+    String serverIpAddress = "",msg = "",str;
+    Handler handler = new Handler();
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_client);
+        chat = (TextView) findViewById(R.id.chat);
+        serverIp = (EditText) findViewById(R.id.server_ip);
+        smessage = (EditText) findViewById(R.id.smessage);
+        sent = (Button) findViewById(R.id.sent_button);
+        sent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Thread sentThread = new Thread(new sentMessage());
+                sentThread.start();
+            }
+        });
+        connectPhones = (Button) findViewById(R.id.connect_phones);
+        connectPhones.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                serverIpAddress = serverIp.getText().toString();
+                if (!serverIpAddress.equals(""))
+                {
+                    Thread clientThread = new Thread(new
+                            ClientThread());
+                    clientThread.start();
+                }
+            }
+        });
+    }
+    class sentMessage implements Runnable
+    {
+        @Override
+        public void run()
+        {
+            try
+            {
+                InetAddress serverAddr =
+                        InetAddress.getByName(serverIpAddress);
+                Socket socket = new Socket(serverAddr, 10000); //create client socket
+                DataOutputStream os = new
+                        DataOutputStream(socket.getOutputStream());
+                str = smessage.getText().toString();
+                str = str + "\n";
+                msg = msg + "Client : " + str;
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        chat.setText(msg);
+                    }
+                });
+                os.writeBytes(str);
+                os.flush();
+                os.close();
+                socket.close();
+            }
+            catch(IOException e)
+            {
+            }
+        }
+    }
+    public class ClientThread implements Runnable
+    {
+        public void run()
+        {
+            try
+            {
+                while(true)
+                {
+                    InetAddress serverAddr =
+                            InetAddress.getByName(serverIpAddress);
+                    Socket socket = new Socket(serverAddr, 10000); //create client socket
+/*******************************************
+ setup i/p streams
+ ******************************************/
+                    DataInputStream in = new
+                            DataInputStream(socket.getInputStream());
+                    String line = null;
+                    while ((line = in.readLine()) != null)
+                    {
+                        msg = msg + "Server : " + line + "\n";
+                        handler.post(new Runnable()
+                        {
+                            @Override
+                            public void run()
+                            {
+                                chat.setText(msg);
+                            }
+                        });
+                    }
+                    in.close();
+                    socket.close();
+                    Thread.sleep(100);
+                }
+            }
+            catch (Exception e)
+            {}
+        }
+    }
+}
